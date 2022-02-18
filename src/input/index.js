b29290fcadb50b920a111419ae6f98c47fb2c715
yanggang <yangglive@icloud.com>
2022-02-18 16:10:41 +0800

    change

--- src/input/index.js
+++ src/input/index.js
@@ -1,38 +1,39 @@
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types";
 
 const Input = (props) => {
 
-    const {addon, style, className, prefixClass, size, startIcon, endIcon, type, ...rest} = props;
+    const { addon, style, className, prefixClass, size, startIcon, endIcon, type, ...rest } = props;
+
     const classSize = size ? " " + prefixClass + "-" + size : ""
-    const hasStartIcon = startIcon ? " " + prefixClass+ "-has-start-icon" : ""
-    const hasEndIcon = endIcon ? " " + prefixClass+ "-has-end-icon" : ""
-    const typeString = (type && type === "text" )? "text" : "password";
+    const hasStartIcon = startIcon ? " " + prefixClass + "-has-start-icon" : ""
+    const hasEndIcon = endIcon ? " " + prefixClass + "-has-end-icon" : ""
+    const typeString = (type && type === "text") ? "text" : "password";
     const disabled = props.disabled ? " disabled" : "";
-    const classNameString = className? className : ""
+    const classNameString = className ? className : ""
 
     const classes = prefixClass + hasStartIcon + hasEndIcon + classNameString;
-    
+
     const handelChange = (e) => {
-        if(props.onChange) {
-            props.onChange(e)
+        if (props.onChange) {
+            props.onChange(e.target.value)
         }
     }
     return <span className={prefixClass + "-root" + classSize + disabled} style={style}>
-        {startIcon?<span className={prefixClass+"-start-icon"}>{startIcon}</span>:null}
-        <input 
-        className={classes}
-        style={style}
-        type={typeString} 
-        onChange={handelChange}
-        {...rest}     
+        {startIcon ? <span className={prefixClass + "-start-icon"}>{startIcon}</span> : null}
+        <input
+            className={classes}
+            style={style}
+            type={typeString}
+            onChange={handelChange}
+            {...rest}
         />
-        {endIcon?<span className={prefixClass+"-end-icon"}>{endIcon}</span>:null}
+        {endIcon ? <span className={prefixClass + "-end-icon"}>{endIcon}</span> : null}
     </span>
 }
 
 Input.propTypes = {
-    type: PropTypes.oneOf(['text','password']),
+    type: PropTypes.oneOf(['text', 'password']),
     size: PropTypes.oneOf(['large', 'default', 'small']),
 };
 
