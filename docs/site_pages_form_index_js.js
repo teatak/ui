8433dceba9a486b34cfd977e1351e59e4e568763
yanggang <yangglive@icloud.com>
2022-02-21 11:24:43 +0800

    change 33

---
+++ docs/site_pages_form_index_js.js
@@ -0,0 +1,110 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunk_teatak_ui"] = self["webpackChunk_teatak_ui"] || []).push([["site_pages_form_index_js"],{
+
+/***/ "./site/components/markdown/index.js":
+/*!*******************************************!*\
+  !*** ./site/components/markdown/index.js ***!
+  \*******************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _babel_runtime_helpers_extends__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/helpers/extends */ \"./node_modules/@babel/runtime/helpers/esm/extends.js\");\n/* harmony import */ var _babel_runtime_helpers_objectWithoutProperties__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @babel/runtime/helpers/objectWithoutProperties */ \"./node_modules/@babel/runtime/helpers/esm/objectWithoutProperties.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react_markdown__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! react-markdown */ \"./node_modules/react-markdown/src/react-markdown.js\");\n/* harmony import */ var react_markdown__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(react_markdown__WEBPACK_IMPORTED_MODULE_3__);\n/* harmony import */ var remark_gfm__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! remark-gfm */ \"./node_modules/remark-gfm/index.js\");\n/* harmony import */ var remark_gfm__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(remark_gfm__WEBPACK_IMPORTED_MODULE_4__);\n/* harmony import */ var react_syntax_highlighter__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! react-syntax-highlighter */ \"./node_modules/react-syntax-highlighter/dist/esm/prism.js\");\n/* harmony import */ var react_syntax_highlighter_dist_esm_styles_prism__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! react-syntax-highlighter/dist/esm/styles/prism */ \"./node_modules/react-syntax-highlighter/dist/esm/styles/prism/coldark-dark.js\");\n\n\nvar _excluded = [\"node\", \"inline\", \"className\", \"children\"];\n\n\n\n\n/* Use `…/dist/cjs/…` if you’re not in ESM! */\n\n\nvar components = {\n  code: function code(_ref) {\n    var node = _ref.node,\n        inline = _ref.inline,\n        className = _ref.className,\n        children = _ref.children,\n        props = (0,_babel_runtime_helpers_objectWithoutProperties__WEBPACK_IMPORTED_MODULE_1__.default)(_ref, _excluded);\n\n    var match = /language-(\\w+)/.exec(className || '');\n    return !inline && match ? /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2__.createElement(react_syntax_highlighter__WEBPACK_IMPORTED_MODULE_5__.default, (0,_babel_runtime_helpers_extends__WEBPACK_IMPORTED_MODULE_0__.default)({\n      style: react_syntax_highlighter_dist_esm_styles_prism__WEBPACK_IMPORTED_MODULE_6__.default,\n      language: match[1],\n      PreTag: \"div\",\n      children: String(children).replace(/\\n$/, '')\n    }, props)) : /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2__.createElement(\"code\", (0,_babel_runtime_helpers_extends__WEBPACK_IMPORTED_MODULE_0__.default)({\n      className: className\n    }, props), children);\n  }\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (function (props) {\n  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2__.createElement(\"div\", {\n    className: \"markdown\"\n  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2__.createElement((react_markdown__WEBPACK_IMPORTED_MODULE_3___default()), {\n    components: components,\n    children: props.children,\n    remarkPlugins: [(remark_gfm__WEBPACK_IMPORTED_MODULE_4___default())]\n  }));\n});\n\n//# sourceURL=webpack://@teatak/ui/./site/components/markdown/index.js?");
+
+/***/ }),
+
+/***/ "./site/pages/form/index.js":
+/*!**********************************!*\
+  !*** ./site/pages/form/index.js ***!
+  \**********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var _src__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../src */ \"./src/index.js\");\n/* harmony import */ var _src_input_style__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../src/input/style */ \"./src/input/style/index.js\");\n/* harmony import */ var _src_button_style__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../../src/button/style */ \"./src/button/style/index.js\");\n/* harmony import */ var _src_form_style__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../../../src/form/style */ \"./src/form/style/index.js\");\n/* harmony import */ var _components_markdown__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../../components/markdown */ \"./site/components/markdown/index.js\");\n/* harmony import */ var _index_css__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./index.css */ \"./site/pages/form/index.css\");\n\n\n\n\n\n\n\n\n\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (function () {\n  var onSubmit = function onSubmit(e, errors, values) {\n    e.preventDefault();\n    console.log(e, errors, values);\n  };\n\n  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"div\", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"div\", {\n    className: \"examples\"\n  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"div\", null, \"default:\"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_src__WEBPACK_IMPORTED_MODULE_1__.Form, {\n    onSubmit: onSubmit\n  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_src__WEBPACK_IMPORTED_MODULE_1__.Form.Item, {\n    name: \"name\",\n    rules: [{\n      required: true,\n      message: '请输入用户名!'\n    }, {\n      type: 'string',\n      message: '请输入5-20位有效的用户名!',\n      pattern: /^.{5,20}$/\n    }]\n  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_src__WEBPACK_IMPORTED_MODULE_1__.Input, {\n    style: {\n      width: \"100%\"\n    }\n  })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_src__WEBPACK_IMPORTED_MODULE_1__.Form.Item, {\n    name: \"password\",\n    rules: [{\n      required: true,\n      message: '请输入密码!'\n    }, {\n      type: 'string',\n      message: '请输入密码5-20位!',\n      pattern: /^.{5,20}$/\n    }]\n  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_src__WEBPACK_IMPORTED_MODULE_1__.Input, {\n    type: \"password\",\n    style: {\n      width: \"100%\"\n    }\n  })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_src__WEBPACK_IMPORTED_MODULE_1__.Form.Item, {\n    name: \"password2\",\n    rules: [{\n      required: true,\n      message: '请输入密码!'\n    }, {\n      type: 'string',\n      message: '请输入密码5-20位!',\n      pattern: /^.{5,20}$/\n    }]\n  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_src__WEBPACK_IMPORTED_MODULE_1__.Input, {\n    type: \"password\",\n    disabled: true,\n    style: {\n      width: \"100%\"\n    }\n  })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_src__WEBPACK_IMPORTED_MODULE_1__.Button, {\n    color: \"primary\",\n    type: \"filled\",\n    htmlType: \"submit\"\n  }, \"Submit\"))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_components_markdown__WEBPACK_IMPORTED_MODULE_5__.default, {\n    children: \"\\n> color: primary,danger\\n```javascript\\nimport React from \\\"react\\\";\\nimport {Button} from \\\"@teatak/ui\\\";\\nimport {\\n    Delete,\\n    PhotoCamera,\\n    AddCircle\\n} from '@material-ui/icons';\\n\\nexport default () => <div className=\\\"examples\\\">\\n    <div>default:</div>\\n</div>    \\n```\\n        \"\n  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_components_markdown__WEBPACK_IMPORTED_MODULE_5__.default, {\n    children: \"\\n## API\\n\\n| Property  | Description | Type | Default |\\n| --------- | ------- | ------- | ------- |\\n| type      | can be set to 'filled','outlined','default' | string | default |\\n| size      | can be set to small large or default | string | default |\\n| rounded   | rounded | boolean | false |\\n| disabled  | disabled state of button\\t | boolean | false |\\n| htmlType  | htmlType | string | button |\\n| onClick   | set the handler to handle click event\\t| (event) => void | - |\\n        \"\n  }));\n});\n\n//# sourceURL=webpack://@teatak/ui/./site/pages/form/index.js?");
+
+/***/ }),
+
+/***/ "./src/button/style/index.js":
+/*!***********************************!*\
+  !*** ./src/button/style/index.js ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_index_less__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../style/index.less */ \"./src/style/index.less\");\n/* harmony import */ var _index_less__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./index.less */ \"./src/button/style/index.less\");\n\n\n\n//# sourceURL=webpack://@teatak/ui/./src/button/style/index.js?");
+
+/***/ }),
+
+/***/ "./src/form/style/index.js":
+/*!*********************************!*\
+  !*** ./src/form/style/index.js ***!
+  \*********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_index_less__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../style/index.less */ \"./src/style/index.less\");\n/* harmony import */ var _index_less__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./index.less */ \"./src/form/style/index.less\");\n\n\n\n//# sourceURL=webpack://@teatak/ui/./src/form/style/index.js?");
+
+/***/ }),
+
+/***/ "./src/input/style/index.js":
+/*!**********************************!*\
+  !*** ./src/input/style/index.js ***!
+  \**********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_index_less__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../style/index.less */ \"./src/style/index.less\");\n/* harmony import */ var _index_less__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./index.less */ \"./src/input/style/index.less\");\n\n\n\n//# sourceURL=webpack://@teatak/ui/./src/input/style/index.js?");
+
+/***/ }),
+
+/***/ "./site/pages/form/index.css":
+/*!***********************************!*\
+  !*** ./site/pages/form/index.css ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://@teatak/ui/./site/pages/form/index.css?");
+
+/***/ }),
+
+/***/ "./src/button/style/index.less":
+/*!*************************************!*\
+  !*** ./src/button/style/index.less ***!
+  \*************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://@teatak/ui/./src/button/style/index.less?");
+
+/***/ }),
+
+/***/ "./src/form/style/index.less":
+/*!***********************************!*\
+  !*** ./src/form/style/index.less ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://@teatak/ui/./src/form/style/index.less?");
+
+/***/ }),
+
+/***/ "./src/input/style/index.less":
+/*!************************************!*\
+  !*** ./src/input/style/index.less ***!
+  \************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://@teatak/ui/./src/input/style/index.less?");
+
+/***/ })
+
+}]);
--- src/input/index.js
+++ src/input/index.js
@@ -1,34 +1,51 @@
 import React, { useState } from "react"
 import PropTypes from "prop-types";
+import classnames from 'classnames'
 
 const Input = (props) => {
 
-    const { addon, style, className, prefixClass, size, startIcon, endIcon, type, ...rest } = props;
+    const { addon, style, disabled, className, prefixClass, size, startIcon, endIcon, type, ...rest } = props;
 
-    const classSize = size ? " " + prefixClass + "-" + size : ""
-    const hasStartIcon = startIcon ? " " + prefixClass + "-has-start-icon" : ""
-    const hasEndIcon = endIcon ? " " + prefixClass + "-has-end-icon" : ""
+    // const hasStartIcon = startIcon ? " " + prefixClass + "-has-start-icon" : ""
+    // const hasEndIcon = endIcon ? " " + prefixClass + "-has-end-icon" : ""
     const typeString = (type && type === "text") ? "text" : "password";
-    const disabled = props.disabled ? " disabled" : "";
-    const classNameString = className ? className : ""
+    // const disabled = disabled ? " disabled" : "";
+    // const classNameString = className ? className : ""
 
-    const classes = prefixClass + hasStartIcon + hasEndIcon + classNameString;
+    // const classes = prefixClass + hasStartIcon + hasEndIcon + classNameString;
 
-    const handleChange = (newValue) => {
-        console.log(newValue)
+    const classNamesRoot = classnames(
+        `${prefixClass}-root`,
+        `${prefixClass}-size-${size}`,
+        {
+            [`disabled`]: disabled
+        }
+    )
+
+    const classNames = classnames(
+        prefixClass,
+        {
+            [`${prefixClass}-has-start-icon`]: startIcon,
+            [`${prefixClass}--has-end-icon`]: endIcon,
+        },
+        className,
+    )
+
+    const handleChange = (newValue, e) => {
         if (props.onChange) {
-            props.onChange(newValue)
+            props.onChange(newValue, e)
         }
     }
-    return <span className={prefixClass + "-root" + classSize + disabled} style={style}>
+    return <span className={classNamesRoot} style={style}>
         {startIcon ? <span className={prefixClass + "-start-icon"}>{startIcon}</span> : null}
         <input
-            className={classes}
+            className={classNames}
             style={style}
             type={typeString}
             onChange={(e) => {
-                handleChange(e.target.value)
+                handleChange(e.target.value, e)
             }}
+            disabled={disabled}
             {...rest}
         />
         {endIcon ? <span className={prefixClass + "-end-icon"}>{endIcon}</span> : null}
@@ -37,11 +54,13 @@ const Input = (props) => {
 
 Input.propTypes = {
     type: PropTypes.oneOf(['text', 'password']),
-    size: PropTypes.oneOf(['large', 'default', 'small']),
+    size: PropTypes.oneOf(['large', 'medium', 'small']),
 };
 
 Input.defaultProps = {
     prefixClass: "tui-input",
     type: "text",
+    size: "medium",
+    disabled: false,
 };
 export default Input
