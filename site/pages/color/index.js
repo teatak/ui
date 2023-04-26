8834ffc89b046eecaf58d1aa11a75e761d9bb93d
Yang <yangglive@icloud.com>
2023-04-26 14:04:23 +0800

    change

--- site/pages/color/index.js
+++ site/pages/color/index.js
@@ -10,16 +10,16 @@ const Color = (props) => {
   const { name } = props
   return (
     <div className="colors">
-      <div style={{ background: `rgb(var(--${name}-1))` }}>--{name}-1</div>
-      <div style={{ background: `rgb(var(--${name}-2))` }}>--{name}-2</div>
-      <div style={{ background: `rgb(var(--${name}-3))` }}>--{name}-3</div>
-      <div style={{ background: `rgb(var(--${name}-4))` }}>--{name}-4</div>
-      <div style={{ background: `rgb(var(--${name}-5))` }}>--{name}-5</div>
-      <div style={{ background: `rgb(var(--${name}-6))`, color: `var(--color-background-2)` }} >--{name}-6</div>
-      <div className="reverse" style={{ background: `rgb(var(--${name}-7))`, color: `var(--color-background-2)` }} >--{name}-7</div>
-      <div className="reverse" style={{ background: `rgb(var(--${name}-8))`, color: `var(--color-background-2)` }} >--{name}-8</div>
-      <div className="reverse" style={{ background: `rgb(var(--${name}-9))`, color: `var(--color-background-2)` }} >--{name}-9</div>
-      <div className="reverse" style={{ background: `rgb(var(--${name}-10))`, color: `var(--color-background-2)` }} >--{name}-10</div>
+      <div style={{ background: `rgb(var(--tt-${name}-1))` }}>--tt-{name}-1</div>
+      <div style={{ background: `rgb(var(--tt-${name}-2))` }}>--tt-{name}-2</div>
+      <div style={{ background: `rgb(var(--tt-${name}-3))` }}>--tt-{name}-3</div>
+      <div style={{ background: `rgb(var(--tt-${name}-4))` }}>--tt-{name}-4</div>
+      <div style={{ background: `rgb(var(--tt-${name}-5))` }}>--tt-{name}-5</div>
+      <div style={{ background: `rgb(var(--tt-${name}-6))`, color: `var(--tt-color-background-2)` }} >--{name}-6</div>
+      <div className="reverse" style={{ background: `rgb(var(--${name}-7))`, color: `var(--tt-color-background-2)` }} >--{name}-7</div>
+      <div className="reverse" style={{ background: `rgb(var(--${name}-8))`, color: `var(--tt-color-background-2)` }} >--{name}-8</div>
+      <div className="reverse" style={{ background: `rgb(var(--${name}-9))`, color: `var(--tt-color-background-2)` }} >--{name}-9</div>
+      <div className="reverse" style={{ background: `rgb(var(--${name}-10))`, color: `var(--tt-color-background-2)` }} >--{name}-10</div>
     </div>
   )
 }
@@ -31,83 +31,83 @@ export default () => {
       <h2>Foreground:</h2>
       <div className="examples">
         <Space>
-          <span style={{ color: `var(--color-foreground-1)` }}>--color-foreground-1</span>
-          <span style={{ color: `var(--color-foreground-2)` }}>--color-foreground-2</span>
-          <span style={{ color: `var(--color-foreground-3)` }}>--color-foreground-3</span>
-          <span style={{ color: `var(--color-foreground-4)` }}>--color-foreground-4</span>
+          <span style={{ color: `var(--tt-color-foreground-1)` }}>--color-foreground-1</span>
+          <span style={{ color: `var(--tt-color-foreground-2)` }}>--color-foreground-2</span>
+          <span style={{ color: `var(--tt-color-foreground-3)` }}>--color-foreground-3</span>
+          <span style={{ color: `var(--tt-color-foreground-4)` }}>--color-foreground-4</span>
         </Space>
       </div>
       <h2>Background:</h2>
       <Row gap="16px" className="bgs">
         <Col xs="24" sm="12" lg="6">
-          <div style={{ background: `var(--color-background-1)` }}>--color-background-1</div>
+          <div style={{ background: `var(--tt-color-background-1)` }}>--color-background-1</div>
         </Col>
         <Col xs="24" sm="12" lg="6">
-          <div style={{ background: `var(--color-background-2)` }}>--color-background-2</div>
+          <div style={{ background: `var(--tt-color-background-2)` }}>--color-background-2</div>
         </Col>
         <Col xs="24" sm="12" lg="6">
-          <div style={{ background: `var(--color-background-3)` }}>--color-background-3</div>
+          <div style={{ background: `var(--tt-color-background-3)` }}>--color-background-3</div>
         </Col>
         <Col xs="24" sm="12" lg="6">
-          <div style={{ background: `var(--color-background-4)` }}>--color-background-4</div>
+          <div style={{ background: `var(--tt-color-background-4)` }}>--color-background-4</div>
         </Col>
       </Row>
       <h2>Border:</h2>
       <Row gap="16px" className="borders">
         <Col xs="24" sm="12" lg="6">
-          <div style={{ border: `solid 4px var(--color-border-1)` }}>--color-border-1</div>
+          <div style={{ border: `solid 4px var(--tt-color-border-1)` }}>--color-border-1</div>
         </Col>
         <Col xs="24" sm="12" lg="6">
-          <div style={{ border: `solid 4px var(--color-border-2)` }}>--color-border-2</div>
+          <div style={{ border: `solid 4px var(--tt-color-border-2)` }}>--color-border-2</div>
         </Col>
         <Col xs="24" sm="12" lg="6">
-          <div style={{ border: `solid 4px var(--color-border-3)` }}>--color-border-3</div>
+          <div style={{ border: `solid 4px var(--tt-color-border-3)` }}>--color-border-3</div>
         </Col>
         <Col xs="24" sm="12" lg="6">
-          <div style={{ border: `solid 4px var(--color-border-4)` }}>--color-border-4</div>
+          <div style={{ border: `solid 4px var(--tt-color-border-4)` }}>--color-border-4</div>
         </Col>
       </Row>
       <h2>Fill:</h2>
       <Row gap="16px" className="bgs">
         <Col xs="24" sm="12" lg="6">
-          <div style={{ background: `var(--color-fill-1)` }}>--color-fill-1</div>
+          <div style={{ background: `var(--tt-color-fill-1)` }}>--color-fill-1</div>
         </Col>
         <Col xs="24" sm="12" lg="6">
-          <div style={{ background: `var(--color-fill-2)` }}>--color-fill-2</div>
+          <div style={{ background: `var(--tt-color-fill-2)` }}>--color-fill-2</div>
         </Col>
         <Col xs="24" sm="12" lg="6">
-          <div style={{ background: `var(--color-fill-3)` }}>--color-fill-3</div>
+          <div style={{ background: `var(--tt-color-fill-3)` }}>--color-fill-3</div>
         </Col>
         <Col xs="24" sm="12" lg="6">
-          <div style={{ background: `var(--color-fill-4)` }}>--color-fill-4</div>
+          <div style={{ background: `var(--tt-color-fill-4)` }}>--color-fill-4</div>
         </Col>
       </Row>
       <h2>Box shadow:</h2>
       <Row gap="16px" className="shadows">
         <Col xs="24" sm="12" lg="6">
-          <div style={{ boxShadow: `var(--box-shadow-1)` }}>--box-shadow-1</div>
+          <div style={{ boxShadow: `var(--tt-box-shadow-1)` }}>--box-shadow-1</div>
         </Col>
         <Col xs="24" sm="12" lg="6">
-          <div style={{ boxShadow: `var(--box-shadow-2)` }}>--box-shadow-2</div>
+          <div style={{ boxShadow: `var(--tt-box-shadow-2)` }}>--box-shadow-2</div>
         </Col>
         <Col xs="24" sm="12" lg="6">
-          <div style={{ boxShadow: `var(--box-shadow-3)` }}>--box-shadow-3</div>
+          <div style={{ boxShadow: `var(--tt-box-shadow-3)` }}>--box-shadow-3</div>
         </Col>
         <Col xs="24" sm="12" lg="6">
-          <div style={{ boxShadow: `var(--box-shadow-4)` }}>--box-shadow-4</div>
+          <div style={{ boxShadow: `var(--tt-box-shadow-4)` }}>--box-shadow-4</div>
         </Col>
 
         <Col xs="24" sm="12" lg="6">
-          <div style={{ boxShadow: `var(--box-shadow-btn)` }}>--box-shadow-btn</div>
+          <div style={{ boxShadow: `var(--tt-box-shadow-btn)` }}>--box-shadow-btn</div>
         </Col>
         <Col xs="24" sm="12" lg="6">
-          <div style={{ boxShadow: `var(--box-shadow-popup)` }}>--box-shadow-popup</div>
+          <div style={{ boxShadow: `var(--tt-box-shadow-popup)` }}>--box-shadow-popup</div>
         </Col>
         <Col xs="24" sm="12" lg="6">
-          <div style={{ boxShadow: `var(--box-shadow-popup-up)` }}>--box-shadow-popup-up</div>
+          <div style={{ boxShadow: `var(--tt-box-shadow-popup-up)` }}>--box-shadow-popup-up</div>
         </Col>
         <Col xs="24" sm="12" lg="6">
-          <div style={{ boxShadow: `var(--box-shadow-popup-down)` }}>--box-shadow-popup-down</div>
+          <div style={{ boxShadow: `var(--tt-box-shadow-popup-down)` }}>--box-shadow-popup-down</div>
         </Col>
       </Row>
       <h2>Color:</h2>
